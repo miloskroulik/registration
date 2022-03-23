d2751ae14470f8b362280d30c75c651249222fb8
John Oltman <john.oltman@sitebasin.com>
2022-03-22 21:21:37 -0500

    Set the attached to column in the list builder.

--- src/RegistrationListBuilder.php
+++ src/RegistrationListBuilder.php
@@ -5,13 +5,32 @@ namespace Drupal\registration;
 use Drupal;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Link;
 use Drupal\registration\Entity\RegistrationType;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines the list builder for registrations.
  */
 class RegistrationListBuilder extends EntityListBuilder {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type):RegistrationListBuilder {
+    $instance = parent::createInstance($container, $entity_type);
+    $instance->entityTypeManager = $container->get('entity_type.manager');
+    return $instance;
+  }
 
   /**
    * {@inheritdoc}
@@ -40,13 +59,23 @@ class RegistrationListBuilder extends EntityListBuilder {
       $user = Link::fromTextAndUrl($user_entity->getDisplayName(), $user_entity->toUrl());
     }
 
+    // Get the attached column value.
+    $attached = '';
+    if (!$entity->get('entity_type_id')->isEmpty() && !$entity->get('entity_id')->isEmpty()) {
+      $storage = $this->entityTypeManager->getStorage($entity->get('entity_type_id')->first()->value);
+      $attached_to_entity = $storage->load($entity->get('entity_id')->first()->value);
+      if ($attached_to_entity) {
+        $attached = Link::fromTextAndUrl($attached_to_entity->label(), $attached_to_entity->toUrl());
+      }
+    }
+
     /** @var \Drupal\registration\Entity\RegistrationInterface $entity */
     $registration_type = RegistrationType::load($entity->bundle());
 
     $row['id'] = Link::fromTextAndUrl($entity->id(), $entity->toUrl());
     $row['type'] = $registration_type->label();
     $row['user'] = $user;
-    $row['attached'] = '';
+    $row['attached'] = $attached;
     $row['status'] = $entity->getState()->label();
     $row['updated'] = Drupal::service('date.formatter')->format($entity->getChangedTime(), 'short');
 
