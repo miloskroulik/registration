d57a6fca3f9701b39d60d8320313e831bc28fa48
John Oltman <john.oltman@sitebasin.com>
2022-05-02 05:54:16 -0500

    Add a cron task for set and forget mode.

---
+++ src/Cron/SetAndForget.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Drupal\registration\Cron;
+
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Queue\QueueFactory;
+use Drupal\Core\Queue\QueueInterface;
+use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
+
+/**
+ * Maintain the per-entity "status" setting automatically.
+ *
+ * This is the "Enabled" checkbox on the Settings form. This cron job keeps
+ * the field in sync with the "Open" and "Close" dates on the same form.
+ *
+ * @see \Drupal\registration\Form\RegistrationSettingsForm
+ * @see \Drupal\registration\Plugin\QueueWorker\SetAndForget
+ */
+class SetAndForget {
+
+  /**
+   * The database service.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected Connection $database;
+
+  /**
+   * The queue.
+   *
+   * @var \Drupal\Core\Queue\QueueInterface
+   */
+  protected QueueInterface $queue;
+
+  /**
+   * Constructs a new SetAndForget object.
+   *
+   * @param \Drupal\Core\Database\Connection $database
+   *   The database.
+   * @param \Drupal\Core\Queue\QueueFactory $queue_factory
+   *   The queue factory.
+   */
+  public function __construct(Connection $database, QueueFactory $queue_factory) {
+    $this->database = $database;
+    $this->queue = $queue_factory->get('registration.set_and_forget');
+  }
+
+  /**
+   * Run this task.
+   */
+  public function run() {
+    // Establish the current time in the storage timezone and format.
+    $storage_format = 'Y-m-d\TH:i:s';
+    $storage_timezone = new \DateTimeZone(DateTimeItemInterface::STORAGE_TIMEZONE);
+    $now = new DrupalDateTime('now', $storage_timezone);
+    $now_date = $now->format($storage_format);
+
+    // Clear existing queue items to avoid reprocessing.
+    $this->queue->deleteQueue();
+
+    // Registrations that should be enabled but aren't.
+    $query = $this->database->select('registration_settings', 'r')
+      ->fields('r')
+      ->condition('status', 0)
+      ->condition('open', $now_date, '<=')
+      ->condition('close', $now_date, '>=');
+    $result = $query->execute();
+
+    foreach ($result as $record) {
+      $item = [
+        'settings_id' => $record->settings_id,
+        'new_status' => 1,
+      ];
+      $this->queue->createItem($item);
+    }
+
+    // Registrations that are enabled but shouldn't be.
+    $query = $this->database->select('registration_settings', 'r')
+      ->fields('r')
+      ->condition('status', 1);
+    $orGroup = $query->orConditionGroup()
+      ->condition('open', $now_date, '>')
+      ->condition('close', $now_date, '<');
+    $query->condition($orGroup);
+    $result = $query->execute();
+
+    foreach ($result as $record) {
+      $item = [
+        'settings_id' => $record->settings_id,
+        'new_status' => 0,
+      ];
+      $this->queue->createItem($item);
+    }
+  }
+
+}
---
+++ src/Plugin/QueueWorker/SetAndForget.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Drupal\registration\Plugin\QueueWorker;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Automatically maintains the "status" field on the per-sentity Settings form.
+ *
+ * @QueueWorker(
+ *  id = "registration.set_and_forget",
+ *  title = @Translation("Set and forget"),
+ *  cron = {"time" = 30}
+ * )
+ */
+class SetAndForget extends QueueWorkerBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * The logger.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected LoggerInterface $logger;
+
+  /**
+   * Constructs a new SetAndForget object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   The logger.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, LoggerInterface $logger) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->entityTypeManager = $entity_type_manager;
+    $this->logger = $logger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): SetAndForget {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('registration.logger')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processItem($data) {
+    /** @var \Drupal\registration\Entity\RegistrationSettings $settings */
+    $settings_storage = $this->entityTypeManager->getStorage('registration_settings');
+    $settings = $settings_storage->load($data['settings_id']);
+    if ($settings?->getSetting('status') != $data['new_status']) {
+      $settings->set('status', $data['new_status']);
+      $settings->save();
+      if (!empty($data['new_status'])) {
+        $this->logger->notice(new TranslatableMarkup('Automatically set status to "enabled" for @type @id.'), [
+          '@type' => $settings->getHostEntityTypeId(),
+          '@id' => $settings->getHostEntityId(),
+        ]);
+      }
+      else {
+        $this->logger->notice(new TranslatableMarkup('Automatically set status to "disabled" for @type @id.'), [
+          '@type' => $settings->getHostEntityTypeId(),
+          '@id' => $settings->getHostEntityId(),
+        ]);
+      }
+    }
+  }
+
+}
