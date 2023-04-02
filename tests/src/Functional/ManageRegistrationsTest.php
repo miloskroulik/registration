fe291ced1d16149b14754f05e7f6769152f94f7d
John Oltman <john.oltman@sitebasin.com>
2023-04-02 13:00:06 -0500

    Add functional tests for settings and manage registrations.

---
+++ tests/src/Functional/ManageRegistrationsTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Drupal\Tests\registration\Functional;
+
+/**
+ * Tests the manage registrations page.
+ *
+ * @group registration
+ */
+class ManageRegistrationsTest extends RegistrationBrowserTestBase {
+
+  /**
+   * Tests registration settings.
+   */
+  public function testManageRegistrations() {
+    $user = $this->drupalCreateUser();
+    $user->set('field_registration', 'conference');
+    $user->set('name', 'Test user');
+    $user->save();
+
+    // No registrants.
+    $this->drupalGet('user/' . $user->id() . '/registrations');
+    $this->assertSession()->pageTextContains('There are no registrants for Test user');
+
+    /** @var \Drupal\registration\RegistrationStorage $registration_storage */
+    $registration_storage = $this->entityTypeManager->getStorage('registration');
+    $registration = $registration_storage->create([
+      'workflow' => 'registration',
+      'state' => 'pending',
+      'type' => 'conference',
+      'entity_type_id' => 'user',
+      'entity_id' => $user->id(),
+      'user_uid' => 1,
+      'count' => 2,
+    ]);
+    $registration->save();
+    $save_registration = $registration;
+
+    $this->drupalGet('user/' . $user->id() . '/registrations');
+    $this->assertSession()->pageTextContains('List of registrations for Test user. 2 spaces are filled.');
+
+    $registration = $registration_storage->create([
+      'workflow' => 'registration',
+      'state' => 'pending',
+      'type' => 'conference',
+      'entity_type_id' => 'user',
+      'entity_id' => $user->id(),
+      'user_uid' => 1,
+      'count' => 1,
+    ]);
+    $registration->save();
+
+    $this->drupalGet('user/' . $user->id() . '/registrations');
+    $this->assertSession()->pageTextContains('List of registrations for Test user. 3 spaces are filled.');
+
+    // Canceled registrations do not count towards the total.
+    $registration = $registration_storage->create([
+      'workflow' => 'registration',
+      'state' => 'canceled',
+      'type' => 'conference',
+      'entity_type_id' => 'user',
+      'entity_id' => $user->id(),
+      'user_uid' => 1,
+      'count' => 1,
+    ]);
+    $registration->save();
+
+    $this->drupalGet('user/' . $user->id() . '/registrations');
+    $this->assertSession()->pageTextContains('List of registrations for Test user. 3 spaces are filled.');
+
+    // Held registrations count towards the total.
+    $registration = $registration_storage->create([
+      'workflow' => 'registration',
+      'state' => 'held',
+      'type' => 'conference',
+      'entity_type_id' => 'user',
+      'entity_id' => $user->id(),
+      'user_uid' => 1,
+      'count' => 1,
+    ]);
+    $registration->save();
+
+    $this->drupalGet('user/' . $user->id() . '/registrations');
+    $this->assertSession()->pageTextContains('List of registrations for Test user. 4 spaces are filled.');
+
+    // Include capacity when configured.
+    $host_entity = $registration->getHostEntity();
+    $settings = $host_entity->getSettings();
+    $settings->set('capacity', 10);
+    $settings->save();
+    $this->drupalGet('user/' . $user->id() . '/registrations');
+    $this->assertSession()->pageTextContains('List of registrations for Test user. 4 of 10 spaces are filled.');
+
+    // Reflect deletions.
+    $registration->delete();
+    $save_registration->delete();
+    $settings->set('capacity', 0);
+    $settings->save();
+    $this->drupalGet('user/' . $user->id() . '/registrations');
+    $this->assertSession()->pageTextContains('List of registrations for Test user. 1 space is filled.');
+
+    // Host entity is not configured for registration.
+    $user->set('field_registration', NULL);
+    $user->save();
+    $this->drupalGet('user/' . $user->id() . '/registrations');
+    $this->assertSession()->statusCodeEquals(403);
+  }
+
+}
---
+++ tests/src/Functional/RegistrationSettingsTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\Tests\registration\Functional;
+
+/**
+ * Tests registration settings.
+ *
+ * @group registration
+ */
+class RegistrationSettingsTest extends RegistrationBrowserTestBase {
+
+  /**
+   * Tests registration settings.
+   */
+  public function testRegistrationSettings() {
+    $user = $this->drupalCreateUser();
+    $user->set('field_registration', 'conference');
+    $user->save();
+
+    // Registration settings can be edited and saved.
+    $this->drupalGet('user/' . $user->id() . '/registrations/settings');
+    $this->getSession()->getPage()->pressButton('Save');
+    $this->assertSession()->pageTextContains('The settings have been saved.');
+
+    // Registration settings cannot be deleted.
+    $this->drupalGet('user/' . $user->id() . '/registrations/settings/delete');
+    $this->assertSession()->statusCodeEquals(404);
+
+    // Registration settings cannot edited if the host entity is not configured
+    // for registration.
+    $user->set('field_registration', NULL);
+    $user->save();
+    $this->drupalGet('user/' . $user->id() . '/registrations/settings');
+    $this->assertSession()->statusCodeEquals(403);
+  }
+
+}
