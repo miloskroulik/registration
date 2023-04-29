2afb12683d2e1350fbd013a31af7f574779aea44
John Oltman <john.oltman@sitebasin.com>
2023-04-29 16:55:26 -0500

    Issue #3356153: Spaces remaining value as Views field.

---
+++ src/Plugin/views/field/HostEntityRegistrationCount.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\registration\Plugin\views\field;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Field handler to present the registration count for a host entity.
+ *
+ * @ViewsField("host_entity_registration_count")
+ */
+class HostEntityRegistrationCount extends FieldPluginBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): HostEntityRegistrationCount {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->entityTypeManager = $container->get('entity_type.manager');
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function clickSortable() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // Leave empty to avoid a query on this field.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    if ($entity = $this->getEntity($values)) {
+      $handler = $this->entityTypeManager->getHandler('registration', 'host_entity');
+      $host_entity = $handler->createHostEntity($entity);
+      if ($host_entity->isConfiguredForRegistration()) {
+        $build = [
+          '#markup' => $host_entity->getRegistrationCount(),
+        ];
+        $host_entity->addCacheableDependencies($build, [$host_entity->getSettings()]);
+        return $build;
+      }
+    }
+
+    return NULL;
+  }
+
+}
---
+++ src/Plugin/views/field/HostEntitySpacesRemaining.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Drupal\registration\Plugin\views\field;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Field handler to present the spaces remaining for a host entity.
+ *
+ * @ViewsField("host_entity_spaces_remaining")
+ */
+class HostEntitySpacesRemaining extends FieldPluginBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): HostEntitySpacesRemaining {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->entityTypeManager = $container->get('entity_type.manager');
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function clickSortable() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // Leave empty to avoid a query on this field.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    if ($entity = $this->getEntity($values)) {
+      $handler = $this->entityTypeManager->getHandler('registration', 'host_entity');
+      $host_entity = $handler->createHostEntity($entity);
+      if ($host_entity->isConfiguredForRegistration()) {
+        $spaces_remaining = $host_entity->getSpacesRemaining();
+        if (!is_null($spaces_remaining)) {
+          $build = [
+            '#markup' => $spaces_remaining,
+          ];
+        }
+        else {
+          $build = [
+            '#markup' => $this->t('Unlimited'),
+          ];
+        }
+        $host_entity->addCacheableDependencies($build, [$host_entity->getSettings()]);
+        return $build;
+      }
+    }
+
+    return NULL;
+  }
+
+}
