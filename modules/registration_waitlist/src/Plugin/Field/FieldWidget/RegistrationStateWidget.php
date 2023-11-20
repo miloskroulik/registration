e29008d0843b7ac4ebe02460a4c2356402d8eb19
John Oltman <john.oltman@sitebasin.com>
2023-11-19 22:02:52 -0600

    Support autofill state on default value form

--- modules/registration_waitlist/src/Plugin/Field/FieldWidget/RegistrationStateWidget.php
+++ modules/registration_waitlist/src/Plugin/Field/FieldWidget/RegistrationStateWidget.php
@@ -2,11 +2,13 @@
 
 namespace Drupal\registration_waitlist\Plugin\Field\FieldWidget;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\registration\Entity\RegistrationSettings;
 use Drupal\registration\Plugin\Field\FieldWidget\RegistrationStateWidget as BaseRegistrationStateWidget;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Extends the implementation of the 'registration_state_default' widget.
@@ -14,9 +16,26 @@ use Drupal\registration\Plugin\Field\FieldWidget\RegistrationStateWidget as Base
 class RegistrationStateWidget extends BaseRegistrationStateWidget {
 
   /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->entityTypeManager = $container->get('entity_type.manager');
+    return $instance;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state): array {
+    /** @var \Drupal\registration\Entity\RegistrationSettings $entity */
     $entity = $items->getEntity();
 
     // The state field for a registration.
@@ -24,14 +43,18 @@ class RegistrationStateWidget extends BaseRegistrationStateWidget {
       return parent::formElement($items, $delta, $element, $form, $form_state);
     }
 
-    // Autofill state cannot be set on the default values form for registration
-    // settings.
-    // @todo Add support for this.
+    // Default value form when adding or editing a registration field.
     if ($form_state->get('default_value_widget')) {
-      return [];
+      return [
+        '#type' => 'select',
+        '#title' => $this->t('Autofill state'),
+        '#description' => $this->t('The state that wait listed registrations should be placed in when slots become available.'),
+        '#options' => $this->getDefaultValueFormStateOptions(),
+        '#default_value' => $this->getDefaultValueFormStateValue($entity),
+      ];
     }
 
-    /** @var \Drupal\registration\Entity\RegistrationSettings $entity */
+    // Adding or editing a registration settings entity.
     return [
       '#type' => 'select',
       '#title' => $this->t('Autofill state'),
@@ -46,6 +69,54 @@ class RegistrationStateWidget extends BaseRegistrationStateWidget {
    */
   public static function isApplicable(FieldDefinitionInterface $field_definition): bool {
     return parent::isApplicable($field_definition) || ($field_definition->getName() === 'registration_waitlist_autofill_state' && $field_definition->getTargetEntityTypeId() === 'registration_settings');
+  }
+
+  /**
+   * Gets the autofill state options for a settings default value form.
+   *
+   * @return array
+   *   The states as an options array of labels keyed by ID.
+   */
+  protected function getDefaultValueFormStateOptions(): array {
+    $options = [];
+
+    $workflows = $this->entityTypeManager
+      ->getStorage('workflow')
+      ->loadByProperties([
+        'type' => 'registration',
+      ]);
+
+    foreach ($workflows as $workflow) {
+      $states = $workflow->getTypePlugin()->getStates();
+      foreach ($states as $id => $state) {
+        /** @var \Drupal\registration\RegistrationState $state */
+        if (!$state->isCanceled() && ($id != 'waitlist')) {
+          $options[$id] = $state->label();
+        }
+      }
+    }
+
+    return $options;
+  }
+
+  /**
+   * Gets the autofill state default value for registration settings.
+   *
+   * @param \Drupal\registration\Entity\RegistrationSettings $entity
+   *   The registration settings entity.
+   *
+   * @return string|null
+   *   The default value, if available.
+   */
+  protected function getDefaultValueFormStateValue(RegistrationSettings $entity): ?string {
+    $default_value = NULL;
+
+    // Default to the previously set value, if available.
+    if (!$entity->get('registration_waitlist_autofill_state')->isEmpty()) {
+      $default_value = $entity->get('registration_waitlist_autofill_state')->first()->getValue()['value'];
+    }
+
+    return $default_value;
   }
 
   /**
