1415a630c1e0a49820c0b75629ff87a0bf694c5d
Jonathan Shaw <20711-jonathanjfshaw@users.noreply.drupalcode.org>
2024-10-09 12:19:00 +0000

    #3464137 by jonathanshaw: Make it possible to customize who can administer registrations

--- modules/registration_admin_overrides/src/RegistrationOverrideChecker.php
+++ modules/registration_admin_overrides/src/RegistrationOverrideChecker.php
@@ -3,6 +3,7 @@
 namespace Drupal\registration_admin_overrides;
 
 use Drupal\Core\Session\AccountInterface;
+use Drupal\registration\Entity\RegistrationInterface;
 use Drupal\registration\HostEntityInterface;
 
 /**
@@ -13,10 +14,13 @@ class RegistrationOverrideChecker implements RegistrationOverrideCheckerInterfac
   /**
    * {@inheritdoc}
    */
-  public function accountCanOverride(?HostEntityInterface $host_entity, AccountInterface $account, string $setting): bool {
+  public function accountCanOverride(?HostEntityInterface $host_entity, AccountInterface $account, string $setting, ?RegistrationInterface $registration = NULL): bool {
     if ($host_entity) {
       $type = $host_entity->getRegistrationTypeBundle();
       $admin = $account->hasPermission("administer registration") || $account->hasPermission("administer $type registration");
+      if ($registration) {
+        $admin = $registration->access('administer', $account);
+      }
       if ($admin && $account->hasPermission('registration override ' . $setting)) {
         return (bool) $host_entity->getRegistrationType()->getThirdPartySetting('registration_admin_overrides', $setting);
       }
