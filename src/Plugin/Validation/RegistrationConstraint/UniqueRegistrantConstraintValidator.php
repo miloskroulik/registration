ea20fd861ba1af2ce1e871e7e3bfc20ac1548a9a
Jonathan Shaw <20711-jonathanjfshaw@users.noreply.drupalcode.org>
2025-01-14 18:45:50 +0000

    #3497734: Refactor the unique registrant constraint validator

--- src/Plugin/Validation/RegistrationConstraint/UniqueRegistrantConstraintValidator.php
+++ src/Plugin/Validation/RegistrationConstraint/UniqueRegistrantConstraintValidator.php
@@ -3,6 +3,7 @@
 namespace Drupal\registration\Plugin\Validation\RegistrationConstraint;
 
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Session\AccountProxy;
 use Drupal\registration\Entity\RegistrationInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -44,7 +45,6 @@ class UniqueRegistrantConstraintValidator extends ConstraintValidator implements
    * {@inheritdoc}
    */
   public function validate($registration, Constraint $constraint) {
-    /** @var UniqueRegistrantConstraint $constraint */
     if ($registration instanceof RegistrationInterface) {
       $host_entity = $registration->getHostEntity();
       if ($settings = $host_entity?->getSettings()) {
@@ -53,113 +53,104 @@ class UniqueRegistrantConstraintValidator extends ConstraintValidator implements
 
           // Multiple registrations per person are not allowed.
           if ($registration->isNewToHost()) {
-
-            // Check the email address when registering an anonymous user.
             if ($email = $registration->getAnonymousEmail()) {
-              if ($host_entity->isRegistrant(NULL, $email)) {
-                $this->context
-                  ->buildViolation($constraint->emailAlreadyRegisteredMessage, [
-                    '%mail' => $email,
-                  ])
-                  ->atPath('anon_mail')
-                  ->setCode($constraint->emailAlreadyRegisteredCode)
-                  ->setCause(t($constraint->emailAlreadyRegisteredCause))
-                  ->addViolation();
-              }
+              $this->validateEmail($registration, $constraint, $email);
             }
-
-            // Check the user account.
-            elseif ($user = $registration->getUser()) {
-              if ($host_entity->isRegistrant($user)) {
-                // Add a violation. The choice of message depends on the
-                // current user, so add a cache context based on the user.
-                $this->context->getCacheableMetadata()->addCacheContexts(['user']);
-
-                if ($user->id() == $this->currentUser->id()) {
-                  $this->context
-                    ->buildViolation($constraint->youAreAlreadyRegisteredMessage)
-                    ->setCode($constraint->youAreAlreadyRegisteredCode)
-                    ->setCause(t($constraint->youAreAlreadyRegisteredCause))
-                    ->addViolation();
-                }
-                else {
-                  $this->context
-                    ->buildViolation($constraint->userAlreadyRegisteredMessage, [
-                      '%user' => $user->getDisplayName(),
-                    ])
-                    ->atPath('user_uid')
-                    ->setCode($constraint->userAlreadyRegisteredCode)
-                    ->setCause(t($constraint->userAlreadyRegisteredCause))
-                    ->addViolation();
-                }
-              }
-            }
-
-            // The user logged in is registering.
             else {
-              // Whether a violation is added or not depends on the logged in
-              // user, so add a cache context based on the user.
-              $this->context->getCacheableMetadata()->addCacheContexts(['user']);
-
-              if ($host_entity->isRegistrant($this->currentUser)) {
-                $this->context
-                  ->buildViolation($constraint->youAreAlreadyRegisteredMessage)
-                  ->setCode($constraint->youAreAlreadyRegisteredCode)
-                  ->setCause(t($constraint->youAreAlreadyRegisteredCause))
-                  ->addViolation();
-              }
+              $this->validateUser($registration, $constraint, $registration->getUser());
             }
           }
           else {
-            /** @var \Drupal\registration\Entity\RegistrationInterface $original */
+            // For an existing registration, validation is only appropriate
+            // when the email or user has changed. An existing registration
+            // should not become invalid because another registration is
+            // created or the allow_multiple setting is changed.
             $original = $this->context->getOriginal();
 
-            // Check email address.
+            /** @var \Drupal\registration\Entity\RegistrationInterface $original */
             if ($registration->getAnonymousEmail() != $original->getAnonymousEmail()) {
               if ($email = $registration->getAnonymousEmail()) {
-                if ($host_entity->isRegistrant(NULL, $email)) {
-                  $this->context
-                    ->buildViolation($constraint->emailAlreadyRegisteredMessage, [
-                      '%mail' => $email,
-                    ])
-                    ->atPath('anon_mail')
-                    ->setCode($constraint->emailAlreadyRegisteredCode)
-                    ->setCause(($constraint->emailAlreadyRegisteredCause))
-                    ->addViolation();
-                }
+                $this->validateEmail($registration, $constraint, $email);
               }
             }
-
-            // Check the user account.
             if ($registration->getUserId() != $original->getUserId()) {
               if ($user = $registration->getUser()) {
-                if ($host_entity->isRegistrant($user)) {
-                  // Add a violation. The choice of message depends on the
-                  // current user, so add a cache context based on the user.
-                  $this->context->getCacheableMetadata()->addCacheContexts(['user']);
-
-                  if ($user->id() == $this->currentUser->id()) {
-                    $this->context
-                      ->buildViolation($constraint->youAreAlreadyRegisteredMessage)
-                      ->setCode($constraint->youAreAlreadyRegisteredCode)
-                      ->setCause(t($constraint->youAreAlreadyRegisteredCause))
-                      ->addViolation();
-                  }
-                  else {
-                    $this->context
-                      ->buildViolation($constraint->userAlreadyRegisteredMessage, [
-                        '%user' => $user->getDisplayName(),
-                      ])
-                      ->atPath('user_uid')
-                      ->setCode($constraint->userAlreadyRegisteredCode)
-                      ->setCause(t($constraint->userAlreadyRegisteredCause))
-                      ->addViolation();
-                  }
-                }
+                $this->validateUser($registration, $constraint, $user);
               }
             }
           }
         }
+      }
+    }
+  }
+
+  /**
+   * Validates the email address.
+   *
+   * @param \Drupal\registration\Entity\RegistrationInterface $registration
+   *   The registration.
+   * @param \Symfony\Component\Validator\Constraint $constraint
+   *   The constraint.
+   * @param string $email
+   *   The email address.
+   */
+  protected function validateEmail(RegistrationInterface $registration, Constraint $constraint, string $email): void {
+    /** @var UniqueRegistrantConstraint $constraint */
+    $host_entity = $registration->getHostEntity();
+    if ($host_entity->isRegistrant(NULL, $email)) {
+      $this->context
+        ->buildViolation($constraint->emailAlreadyRegisteredMessage, [
+          '%mail' => $email,
+        ])
+        ->atPath('anon_mail')
+        ->setCode($constraint->emailAlreadyRegisteredCode)
+        ->setCause(t($constraint->emailAlreadyRegisteredCause))
+        ->addViolation();
+    }
+  }
+
+  /**
+   * Validates the user account.
+   *
+   * @param \Drupal\registration\Entity\RegistrationInterface $registration
+   *   The registration.
+   * @param \Symfony\Component\Validator\Constraint $constraint
+   *   The constraint.
+   * @param \Drupal\Core\Session\AccountInterface|null $account
+   *   (optional) The user account, defaults to the current user.
+   */
+  protected function validateUser(RegistrationInterface $registration, Constraint $constraint, ?AccountInterface $account = NULL): void {
+    // Default to the current user.
+    if (!$account) {
+      $account = $this->currentUser;
+      // Whether a violation is added or not depends on the logged-in
+      // user, so add a cache context based on the user.
+      $this->context->getCacheableMetadata()->addCacheContexts(['user']);
+    }
+    $host_entity = $registration->getHostEntity();
+
+    /** @var UniqueRegistrantConstraint $constraint */
+    if ($host_entity->isRegistrant($account)) {
+      // The choice of message depends on the current user, so add a cache
+      // context based on the user.
+      $this->context->getCacheableMetadata()->addCacheContexts(['user']);
+
+      if ($account->id() == $this->currentUser->id()) {
+        $this->context
+          ->buildViolation($constraint->youAreAlreadyRegisteredMessage)
+          ->setCode($constraint->youAreAlreadyRegisteredCode)
+          ->setCause(t($constraint->youAreAlreadyRegisteredCause))
+          ->addViolation();
+      }
+      else {
+        $this->context
+          ->buildViolation($constraint->userAlreadyRegisteredMessage, [
+            '%user' => $account->getDisplayName(),
+          ])
+          ->atPath('user_uid')
+          ->setCode($constraint->userAlreadyRegisteredCode)
+          ->setCause(t($constraint->userAlreadyRegisteredCause))
+          ->addViolation();
       }
     }
   }
